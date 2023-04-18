8c94d31626be56e163283a096f03b65084b0d67d
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-04-18 18:57:38 +1000

    Fixed hardcoded directory

--- FileManager.py
+++ FileManager.py
@@ -1,67 +1,42 @@
 import os
 import shutil
-import sys
-import pathlib
 import subprocess
 
-# Define the global variable for filepath
-filepath = ""
-filetype = ""
-
-def moveFile(foundfile=None):
-    if not foundfile:
-        return
-
-    while True:
-        createFolder = input("Create a new Folder (Y/N) : ").upper()
+def moveFile(filepath):
+    if filepath:
+        print("File found at: " + filepath)
         print("---" * 30)
-        if "YES" in createFolder or createFolder == "Y":
-            while True:
-                folder_name = input("Enter folder name to create: ")
-                folder_dest_path = os.path.join("/Users/tristan/", folder_name)
-                if not os.path.exists(folder_dest_path):
-                    os.makedirs(folder_dest_path)
-                    print("Folder created successfully!")
-                    print("---" * 30)
-                    break
-                else:
-                    print("Folder already exists. Please choose a different name.")
-                    print("---" * 30)
-
-        elif "NO" in createFolder or createFolder == "N":
-            ParentName = input("What is the parent of this folder : ")
-            print("---" * 30)
-            FindFolder = input("Enter folder name to move file into : ")
-            print("---" * 30)
-            folder_dest_path = os.path.join("/Users/tristan/", ParentName, FindFolder)
-            print("destination path", folder_dest_path)
-            verify = input("Are you sure you want to do this? (Y/N) : ").upper()
-            print("---" * 30)
-
-            if verify == "YES" or verify == "Y" or verify == "y":
+        choice = input("What do you want to do with the file? (move/open/search) ").lower()
+        if choice == "move":
+            parent_folder = input("What is the parent of this folder: ")
+            folder_name = input("Enter folder name to move file into: ")
+            destination_path = os.path.join(os.path.expanduser("~"), parent_folder, folder_name)
+            print("Destination path: " + destination_path)
+            confirm = input("Are you sure you want to do this? (Y/N): ").lower()
+            if confirm == "y":
                 try:
-                    shutil.move(foundfile, folder_dest_path)
+                    os.makedirs(destination_path, exist_ok=True)
+                    shutil.move(filepath, os.path.join(destination_path, os.path.basename(filepath)))
                     print("File moved successfully!")
-                    break
-
-                except shutil.Error as e:
-                    print("Error while moving file:", e)
-                    break
-
                 except Exception as e:
-                    print("Error:", e)
-                    break
-
+                    print("Error: " + str(e))
+            else:
+                print("File not moved.")
+        elif choice == "open":
+            openFile(filepath)
+        elif choice == "search":
+            pass
         else:
-            print("Please ensure that you answer either yes or no.")
-            print("---" * 30)
+            print("Invalid choice. Please choose from 'move', 'open', or 'search'.")
+    else:
+        print("File not found.")
 
 def openFile(found_file=None):
     if not found_file:
         found_file = input("Enter the file name to search and open: ")
 
     file_found = False
-    for dirpath, dirnames, filenames in os.walk('/Users/tristan/'):  # Update the base directory to search for files
+    for dirpath, dirnames, filenames in os.walk(os.path.expanduser("~")):  # Update the base directory to search for files
         for file in filenames:
             if found_file in file:
                 found_file = os.path.join(dirpath, file)
@@ -98,31 +73,38 @@ def searchComputer():
         return None
 
 # Update the code to set the filepath variable before calling moveFile or openFile
-manageFile = input("Enter 'move' to move a file, 'open' to search and open a file, or 'search' to search the entire computer: ")
 
-while True:
-    if "move" in manageFile:
-        filepath = input("Enter the file path: ")
-        filetype = input("Enter the file type: ")
-        filename = input("Enter the file name: ")
-        moveFile()
-    elif "open" in manageFile:
-        filepath = input("Enter the file path: ")
-        filetype = input("Enter the file type: ")
-        filename = input("Enter the file name: ")
-        openFile(found_file)
-    elif "search" in manageFile:
-        found_file = searchComputer()
-        while found_file:
-            manageFile = input("What do you want to do with the file? (move/open/search)")
-            if "move" in manageFile:
-                moveFile(found_file)
-            elif "open" in manageFile: 
-                openFile(found_file)
-            elif "search" in manageFile:
-                found_file = searchComputer()
-            else:
-                print("Invalid input. Please enter 'move', 'open', or 'search'.")
-    else:
-        print("Invalid input. Please try again .")
+def main():
+    global filepath
+    while True:
+        print("1. Move File")
+        print("2. Open File")
+        print("3. Search Computer")
+        print("4. Exit")
+        choice = input("Enter your choice (1/2/3/4): ")
+        print("---" * 30)
+        
+        if choice == "1":
+                filepath = searchComputer()
+                moveFile(filepath)
+                filepath = ""
+
+        elif choice == "2":
+            filepath = searchComputer()
+            openFile(filepath)
+            filepath = ""
+
+        elif choice == "3":
+            filepath = searchComputer()
+
+        elif choice == "4":
+            print("Exiting...")
+            break
+
+        else:
+            print("Invalid choice. Please choose from 1, 2, 3, or 4.")
+            print("---" * 30)
+
+if __name__ == "main":
+    main()
 
