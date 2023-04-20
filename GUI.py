8f43c993d15efdaf36f750b3b44e9fc42c920ebf
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-04-20 21:18:20 +1000

    Added classes

--- GUI.py
+++ GUI.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import PySimpleGUI as sg # for GUI interface
 import subprocess, os, platform
 import pathlib
@@ -64,3 +65,116 @@ while True:
             sg.popup_error(f"Error:{e}",'Please re-input file name and search before opening.')
         
 window.close()
+=======
+import sys
+from PyQt6.QtCore import QDir, QFile, QIODevice, QTextStream
+from PyQt6.QtWidgets import (
+    QApplication,
+    QLabel,
+    QMainWindow,
+    QStatusBar,
+    QToolBar,
+    QFileDialog,
+    QTreeWidget,
+    QTreeWidgetItem,
+)
+
+class Window(QMainWindow):
+    def __init__(self):
+        super().__init__(parent=None)
+        self.setWindowTitle("File Manager")
+        self._createWidgets()
+        self._createMenu()
+        self._createToolBar()
+        self._createStatusBar()
+
+    def _createWidgets(self):
+        # Tree View
+        self.tree_view = QTreeWidget()
+        self.tree_view.setHeaderLabel("Name")
+        self.setCentralWidget(self.tree_view)
+
+    def _createMenu(self):
+        menu = self.menuBar().addMenu("&File")
+
+        # Open action
+        open_action = menu.addAction("&Open", self._open_file)
+        open_action.setShortcut("Ctrl+O")
+
+        # Save action
+        save_action = menu.addAction("&Save", self._save_file)
+        save_action.setShortcut("Ctrl+S")
+
+        # Save As action
+        save_as_action = menu.addAction("Save &As", self._save_file_as)
+        save_as_action.setShortcut("Ctrl+Shift+S")
+
+        menu.addSeparator()
+
+        # Exit action
+        exit_action = menu.addAction("&Exit", self.close)
+        exit_action.setShortcut("Ctrl+Q")
+
+    def _createToolBar(self):
+        tools = QToolBar()
+        tools.addAction("Exit", self.close)
+        self.addToolBar(tools)
+
+    def _createStatusBar(self):
+        status = QStatusBar()
+        status.showMessage("I'm the Status Bar")
+        self.setStatusBar(status)
+
+    def _open_file(self):
+    	file_path, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*.*);;Text Files (*.txt)")
+		if file_path:
+        	# Clear the tree view
+        	self.tree_view.clear()
+            root_item = QTreeWidgetItem()
+            root_item.setText(0, file_path)
+            self.tree_view.addTopLevelItem(root_item)
+            self._populate_tree_view(file_path, root_item)
+
+    def _populate_tree_view(self, directory, parent_item):
+        dir = QDir(directory)
+        for entry in dir.entryList(QDir.Filter.AllEntries | QDir.Filter.NoDotAndDotDot):
+            item = QTreeWidgetItem()
+            item.setText(0, entry)
+            parent_item.addChild(item)
+
+            entry_path = dir.filePath(entry)
+            if dir.exists(entry_path) and dir.isDir(entry_path):
+                self._populate_tree_view(entry_path, item)
+
+	def _save_file(self):
+		current_item = self.tree_view.currentItem()
+		if current_item is not None:
+			file_path, _ = QFileDialog.getSaveFileName(self, "Save File", current_item.text(0), "Text Files (*.txt)")
+			if file_path:
+				# Save the file
+				with QFile(file_path) as file:
+					if file.open(QIODevice.OpenModeFlag.WriteOnly | QIODevice.OpenModeFlag.Text):
+						stream = QTextStream(file)
+						text = current_item.text(0)
+						stream << text
+						file.close()
+
+    def _save_file_as(self):
+		current_item = self.tree_view.currentItem()
+		if current_item is not None:
+			file_path, _ = QFileDialog.getSaveFileName(self, "Save File", current_item.text(0), "Text Files (*.txt)")
+			if file_path:
+				# Save the file
+				with QFile(file_path) as file:
+					if file.open(QIODevice.OpenModeFlag.WriteOnly | QIODevice.OpenModeFlag.Text):
+						stream = QTextStream(file)
+						text = current_item.text(0)
+						stream << text
+						file.close()
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = Window()
+    window.show()
+    sys.exit(app.exec())
+>>>>>>> Stashed changes
