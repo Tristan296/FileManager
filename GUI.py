238f4eec43e26d70bd430eabf2838ddcc32d43ab
Tristan N <109927879+Tristan296@users.noreply.github.com>
2023-04-24 20:34:49 +1000

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -21,18 +21,17 @@ def open_file(findPath):
     else:                                   # linux variants
         subprocess.call(('xdg-open', findPath))
 
-sg.theme('DarkGrey7')
+sg.theme('SystemDefaultForReal')
 
 file_types = ['exe', 'txt', 'jpg', 'png', 'docx', 'pdf', 'mp4']
 
-layout = [  [sg.Text('Input File Name') , sg.InputText()],
-            [sg.Combo(file_types, size=(20, 6))],
-            [sg.Text('Search Options:')],
-            [sg.Button('Computer')], # Browsing Lets you select a directory to search in.
-            [sg.Text('Enter Path'), sg.InputText()],
+layout = [  [sg.Text('File Name:') , sg.InputText(key='-FILENAME-')],
+            [sg.Text('File Type:'), sg.Combo(file_types, size=(20, 6), key='-FILETYPE-'), sg.Button('Browse')],
+            [sg.Button('Search')],
+            [sg.Listbox(values=[], size=(50, 20), key='-FILELIST-')],
             [sg.Text('Select Function:')],
             [sg.Button('Open'), sg.Button('Move'), sg.Button('Duplicate'), sg.Button('Delete')],
-            [sg.Button('Ok'), sg.Button('Cancel')]]
+            [sg.Button('Cancel')]]
 
 # Create the Window
 window = sg.Window('FileManager', layout)
@@ -43,33 +42,29 @@ searcher = FileSearcher("/")
 # Event Loop to process "events" and get the "values" of the inputs
 while True:
     event, values = window.read()
-    filename = values[0]
-    filetype = values[1]
-    filepath = values[2]
+    filename = values['-FILENAME-']
+    filetype = values['-FILETYPE-']
     if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
         break
     # if the 'Computer' Button is pressed.
-    if event == 'Computer': 
+    if event == 'Search': 
         findPath = searcher.search_file(filename, filetype)
         if findPath == None: 
             sg.popup("Your file couldn't be found, Please try again.")
         else: 
             sg.popup_ok(f"Your file was found at {findPath}")
 
-    elif event == 'OK': 
-        # Search for the file in the specified directory
-        findPath = searcher.search_file(filename, filetype)
-        if findPath is None:
-            sg.popup_error("File not found")
-        else:
-            print(f"Found the file with the following path: {findPath}")
-            print("---" * 30)
-        
+    elif event == 'Browse':
+        folder_path = sg.popup_get_folder('Select a folder to search in')
+        if folder_path:
+            searcher = FileSearcher(folder_path)
+
     elif event == 'Open': 
+        selected_file = values['-FILELIST-'][0]
         try: 
-            open_file(findPath)
+            open_file(selected_file)
         
-        except NameError as e:
-            sg.popup_error(f"Error:{e}",'Please re-input file name and search before opening.')
-        
+        except IndexError:
+            sg.popup_error('Please select a file to open.')
+
 window.close()
